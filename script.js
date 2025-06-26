52d7479b010e0fd8f77b7895ca0c7a571de5947d
DavasProgramacoes <davilucaspetry@gmail.com>
2025-06-26 10:27:33 -0300

    .

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Vocẽ está em sua casa descansando tranquilamente, o que você faz",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "faz nada",
+                afirmacao: "Cagou para a campainha tocando. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "abriu a porta",
+                afirmacao: "abriu a porta e era uma testemunha de jeová"
             }
         ]
     },

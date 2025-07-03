84dd33600085290204af3d6b568795558349da9a
EnzoSielski <141641626+EnzoSielski@users.noreply.github.com>
2025-07-03 09:56:42 -0300

    .

--- script.js
+++ script.js
@@ -32,15 +32,15 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Conforme você se desenvolve no jogo, você acaba desbloquiando as variantes normais de cada arma, sendo em especifico armas explosivas. Qual sua opinião sobre as armas explosivas?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Armas de Explosão são mais empolgantes e legais de usar, fazendo um enorme estrago.",
+                afirmacao: "Gosta de destruição e fogo, aniquilando varios inimigos fracos com apenas um tiro, mas sofre para matar alguns bosses."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Armas de Explosão são pouco precisas e não deixam ter o foco nescessario para acertar tiro precisos.",
+                afirmacao: "Se contenta com o básico e simples, buscando a precisão em tiros acertivos, provavelmente tem problemas em lidar com grandes quantidades de inimigos."
             }
         ]
     },

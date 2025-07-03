28374dbca9c789527dd2761fdd517cafa2eadd67
DavasProgramacoes <davilucaspetry@gmail.com>
2025-07-03 10:23:07 -0300

    .

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Vocẽ está em sua casa descansando tranquilamente, o que você faz",
+        enunciado: "Você encontra Fausto silva em uma cafeteria, o que você faz?",
         alternativas: [
             {
-                texto: "faz nada",
-                afirmacao: "Cagou para a campainha tocando. "
+                texto: "Diz oi",
+                afirmacao: "Fausto Retribui a atitude lhe dizendo um oi acalorado, você se enche de tesão e se pergunta se achou o homen da sua vida."
             },
             {
-                texto: "abriu a porta",
-                afirmacao: "abriu a porta e era uma testemunha de jeová"
+                texto: "Lasca um beijão de Lingua",
+                afirmacao: "Fausto Silva Parece um pouco envergonhado com a situação,mas não encomodado o suficiente para tirar a mão da sua coxa. " ,
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Fausto Parece reciproco com sua atitude ",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "chamar ele para sair",
+                afirmacao: "Fausto aceita na hora, Vocês se encontram ali na cafeteria, Você pediu um ovo cuzido e Fausto pediu o mesmo, conversa vai, conversa vem e Fauto disse 'precisamos apimentar a situação' e Vocês se dirigem ao motel local"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "levar ele para o motel",
+                afirmacao: "Só se for já seu safado, e vocês se dirigem ao motel"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "após seu encontro inesquecivel Fausto Lhe faz uma pergunta 'e se o Galvão Bueno descobrir?'",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Convida ele pro trisal",
+                afirmacao: "Galvão aparece no motel junto com Willan Bonner e pega vocês no flagra, e você convida eles para um menage
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "ele nunca saberá",
+                afirmacao: "Você manteve segredo sobre sua relação com fausto mas Galvão reconheceu o cheiro da calcinha de Fausto pois estava com Willian Bonner no Quarto ao lado"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "",
+                afirmacao: ""
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "",
+                afirmacao: "",
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "",
+                afirmacao: ""
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "",
+                afirmacao: ""
             }
         ]
     },

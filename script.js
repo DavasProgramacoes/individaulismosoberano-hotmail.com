6f0224a4903188d6bc92991c3c92df32dab9cbce
andrefvp <161324681+andrefvp@users.noreply.github.com>
2025-08-21 10:06:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,171 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Quando você chega em casa, recebe a noticia que o melhor casal brasileiro infelizmente se separou. Zé Felipe e Virgina acabaram de se divorciar, oq você faz em relação a isso?",
+        enunciado: "Você encontra Fausto silva em uma cafeteria, o que você faz?",
         alternativas: [
             {
-                texto: "Eu me acabo em chorar, como vão ficar as Marias??? Acho que não acredito mais no amor...",
-                afirmacao: "Chorou. "
+                texto: "Diz oi",
+                afirmacao: "Fausto calorosamente diz oi,"
             },
             {
-                texto: "Ok.",
-                afirmacao: "quem é virgina?"
+                texto: "",
+                afirmacao: "" ,
+            }
+        ]
+    },
+    {
+        enunciado: "então você diz",
+        alternativas: [
+            {
+                texto: "como você está belo hoje",
+                afirmacao: "ele aceita seu elogio e diz 'muito obrigado Senpai UwU' você também está otimo, "
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+    {
+        enunciado: "Então Fausto disse: aceitas tomar uma xicara de ovo?",
+        alternativas: [
+            {
+                texto: "com todo Prazer",
+                afirmacao: "então ele o convida gentilmente para tomar uma xicara de ovo, ",
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+    {
+        enunciado: "enquanto estão na cafeteria ele solta a seguinte frase : 'você é tão bonito que quase faz meu coração parar(literalmente)'",
+        alternativas: [
+            {
+                texto: "me sinto igualmente",
+                afirmacao: "e enquanto conversam, você começa a se sentir diferente, com se estivesse queimando por dentro,"
+            },
+            {
+                texto: "",
+                afirmacao: "",
+            }
+        ]
+    },
+    {
+        enunciado: "'gostaria de ter te conhecido antes gostaria de ir à algum lugar em especial ?'",
+        alternativas: [
+            {
+                texto: "eu adoraria",
+                afirmacao: "logo depois Fausto Silva te leva passear no projac da globo, "
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+    {
+        enunciado: "ele o leva ao projac da globo e enquanto passeavam ele diz: 'parece que você tá pegando fogo bixo'",
+        alternativas: [
+            {
+                texto: "'chama bombeiro lá'que tô pegando fogo ",
+                afirmacao: "e enquanto passeavam Fausto diz que 'parece que você tá pegando fogo bixo' e fico ainda mais apaixonado pelos seus jargões profundos e sentimentais,"
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+    {
+        enunciado: "então ele diz: 'sei que não parece o melhor momento mas....eu tenho um ex.....'",
+        alternativas: [
+            {
+                texto: "Não importa desde que fiquemos juntos <3",
+                afirmacao: "então você descobre que o Fausto possui um ex mas não dá muita importancia afinal ele é o grande amor da sua vida,"
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+      {
+        enunciado: "ele é meio possessivo mas você deve conhece-lo a esse ponto...",
+        alternativas: [
+            {
+                texto: "quem era o seu antigo amor?",
+                afirmacao: "mas mesmo assim você questiona quem era o antigo amor da vida de Fausto,"
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+      {
+        enunciado: "Galvâo Bueno....ele me traiu com o Willian Bonner e tudo esteve acabado desde então",
+        alternativas: [
+            {
+                texto: "Ele não te merecia, você é melhor que isso",
+                afirmacao: "e então você descobre que quem era o antigo amor da vida de Fausto era Galvão Bueno, e de sua traição com Willian Bonner, e diz a Fausto que ele não merecia tanto sofrimento,"
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+     {
+        enunciado: "'Fico feliz por me apoiar mas... já está anoitecendo, você quer ir à algum lugar para passar esta linda noite?'",
+        alternativas: [
+            {
+                texto: "aonde você quiser",
+                afirmacao: "Fausto aceita seu apoio e o convida para passar a noite em um lugar especial,"
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+     {
+        enunciado: "vocês se dirigem ao motel e fausto diz:'este é o melhor motel da cidade com direito a pole dance,strip tease e folhinha verde'",
+        alternativas: [
+            {
+                texto: "Vamos entrando",
+                afirmacao: "ao chegar ao local você descobre ser um dos melhores e mais caros moteis da cidade,"
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+     {
+        enunciado: "quando chegam ao quarto ele diz'solta essa fera ae meu'",
+        alternativas: [
+            {
+                texto: "Oloko bixo, chama bombeiro lá",
+                afirmacao: "e então vocês ficam finalmente juntos sozinhos pela primeira vez,"
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
+    {
+        enunciado: "então inesperadamente com uma bicuda na porta do quarto ele....nada mais nada menos que ele....Galvão Bueno invade a privacidade de vocês e disse 'amigos da rede globo o que estou vendo aqui se não é Fausto Silva metendo'Oloko Bixo'!!' ",
+        alternativas: [
+            {
+                texto: "cala aboca Galvão",
+                afirmacao: "galvão invade a porta do quarto e pega vocês dois na intimidade e você diz que isso não é da conta dele,"
+            },
+            {
+                texto: "cala a boca Galvão",
+                afirmacao: "galvão invade a porta do quarto e pega vocês dois na intimidade e você diz que isso não é da conta dele,"
             }
         ]
     },
@@ -22,51 +178,25 @@ const perguntas = [
         enunciado: "",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "",
+                afirmacao: ""
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "",
+                afirmacao: ""
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "",
+                afirmacao: ""
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "",
+                afirmacao: ""
             }
         ]
     },
